edec49321df9c401da122bdac1d5e1584656c1d7
Jackson Lessa <jackson@magicwebdesign.com.br>
2021-03-11 08:48:21 -0300

    init

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '45deba72b2976329935808d470a7dd34'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'f419ce19-f4b3-4164-b629-4cf8fd4916a1'
+};
